4d9638c0765cf2a79897e01476be71037c471d42
0rphee <0rph3e@proton.me>
2023-01-16 18:40:50 -0600

    basic interpreter functionality implemented

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,17 +1,19 @@
-{-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE TupleSections #-}
-{-# OPTIONS_GHC -Wno-unused-matches #-}
-module Lib where
-
-import Prelude hiding ( replicate )
+module Lib
+  (
+    helloWorld
+  ) where
 
 import qualified Data.Vector as V
-import Data.Word (Word8, Word16)
-import Data.Maybe (fromMaybe)
-import Control.Monad.Trans.State.Lazy ( StateT(StateT) )
+import Data.Word ( Word8 )
+import Data.Maybe ( fromMaybe )
 import Control.Exception ( throw, Exception )
-import Data.List (replicate)
+import Control.Monad ( void )
+-- import Debug.Trace ( trace )
 
+-- =====================================================================
+-- TYPES 
+
+-- | Exceptions used when the program is interpreted
 data BrainhuckException
   = InexistentCellValueException
   | NoMatchingBracketException
@@ -19,13 +21,19 @@ data BrainhuckException
 
 instance Exception BrainhuckException
 
-type Pointer = Word16
+type Pointer = Int
 type MemoryCell = Word8
 
 data ProgramState
-  = MkState String (V.Vector MemoryCell) Pointer
+  = MkState String        -- ^ The program to be executed
+    (V.Vector MemoryCell) -- ^ The memory
+    Pointer               -- ^ The pointer to the memory
   deriving Show
 
+-- =====================================================================
+-- Brainfuck operations 
+
+-- | Primitive function to modify the `Pointer` in `ProgramState`
 pointerOperation :: (Pointer -> Pointer) -> ProgramState -> ProgramState
 pointerOperation op (MkState str mem ptr) = MkState str mem (op ptr)
 
@@ -35,13 +43,14 @@ incPointer = pointerOperation (+1)
 decPointer :: ProgramState -> ProgramState
 decPointer = pointerOperation (\x -> x - 1)
 
+-- Helper Function
 getCurrCellValue :: V.Vector a -> Int -> a
-getCurrCellValue mem intPtr = fromMaybe (throw InexistentCellValueException) (mem V.!? intPtr)
+getCurrCellValue mem ptr = fromMaybe (throw InexistentCellValueException) (mem V.!? ptr)
 
+-- | Primitive function to modify the `MemoryCell` currently pointed at in `ProgramState`
 cellOperation :: (MemoryCell -> MemoryCell) -> ProgramState -> ProgramState
-cellOperation op (MkState str mem ptr) = MkState str (mem V.// [(intPtr, op cellValue)]) ptr
-  where intPtr = fromIntegral ptr
-        cellValue = getCurrCellValue mem intPtr
+cellOperation op (MkState str mem ptr) = MkState str (mem V.// [(ptr, op cellValue)]) ptr
+  where cellValue = getCurrCellValue mem ptr
 
 
 incCell :: ProgramState -> ProgramState
@@ -52,35 +61,36 @@ decCell = cellOperation (\x -> x - 1)
 
 pChar :: ProgramState -> IO ProgramState
 pChar (MkState str mem ptr) = do
-  let intPtr = fromIntegral ptr
-  let cellValue = fromIntegral $ fromMaybe (throw InexistentCellValueException) (mem V.!? intPtr)
-  putChar $ toEnum cellValue -- converts to char the cellValue (int)
-  putStrLn ""
+  let cellValue = getCurrCellValue mem ptr
+  putChar $ (toEnum . fromIntegral) cellValue -- converts to char the cellValue (int)
   pure $ MkState str mem ptr
 
 gChar :: ProgramState -> IO ProgramState
 gChar (MkState str mem ptr) = do
   readCharValue <- fromIntegral . fromEnum <$> getChar
   putChar '\n'
-  let intPtr = fromIntegral ptr
-  let modifiedMem = mem V.// [(intPtr, readCharValue)]
+  let modifiedMem = mem V.// [(ptr, readCharValue)]
   pure $ MkState str modifiedMem ptr
 
 currentCellIsZero :: ProgramState -> Bool
 currentCellIsZero (MkState _ mem ptr) = cellValue == 0
-  where intPtr = fromIntegral ptr
-        cellValue = fromMaybe (throw InexistentCellValueException) (mem V.!? intPtr)
+  where cellValue = getCurrCellValue mem ptr
+
+-- myTrace :: Show a => String -> a -> a
+-- myTrace string other = trace (string <> show other) other 
 
 loop :: String -> ProgramState -> IO ProgramState
-loop _     (MkState [] _ _) = throw NoMatchingBracketException
-loop carry (MkState (']':xs) mem ptr)
--- modify all of the function
-  | endLoop = pure midProgState
-  | otherwise = finalProgState >>= loop (carry++[x]) -- TODO
-  where midProgState = MkState xs mem ptr
-        finalProgState = getProgramOperation x midProgState
-        endLoop = x == ']' && currentCellIsZero midProgState
+loop carry pState =
+  case pState of                                                    -- myTrace "pState in loop: "
+    MkState [] _ _           -> throw NoMatchingBracketException
+    cst@(MkState (']':xs) mem ptr) ->                               -- trace "\tinside (]:xs)" $ 
+      if currentCellIsZero cst                                      -- myTrace "\tTerminaLoop: " $
+      then execute $ MkState xs mem ptr
+      else loop "" $ MkState (reverse carry <> "]" <> xs) mem ptr
+    MkState (x:xs) mem ptr   ->                                     -- trace "\tinside (x:xs)"
+      getProgramOperation x (MkState xs mem ptr) >>= loop (x:carry)
 
+-- | The ']' command is "embeded" in the loop function
 getProgramOperation :: Char -> (ProgramState -> IO ProgramState)
 getProgramOperation char = case char of
   '>' -> pure . incPointer
@@ -89,33 +99,31 @@ getProgramOperation char = case char of
   '-' -> pure . decCell
   ',' -> gChar
   '.' -> pChar
-  '[' -> undefined -- TODO: implement loop
+  '[' -> loop ""
   _   -> pure
--- execute :: ProgramState -> IO ( (), ProgramState )
--- execute :: StateT ProgramState IO ()
--- execute = StateT $  \ case
---     MkState [] mem ptr     -> pure ( (), MkState [] mem ptr )
---     MkState (x:xs) mem ptr -> let nextProgState = getProgramOperation x $ MkState xs mem ptr
---                               in  ( (), ) <$> nextProgState
 
 execute :: ProgramState -> IO ProgramState
 execute (MkState [] mem ptr) = pure $ MkState [] mem ptr
-execute (MkState (x:xs) mem ptr) = let nextProgState = getProgramOperation x $ MkState xs mem ptr
-                                    in nextProgState >>= execute
+execute (MkState (x:xs) mem ptr) = nextProgState >>= execute
+  where nextProgState = getProgramOperation x $ MkState xs mem ptr
 
-initialState :: String -> ProgramState
-initialState str = MkState str cells 0
-  where cells = V.replicate 30 0 -- array size of 30,0000 memory blocks of 1 byte, initialized to 0
+-- | Returns an initial `ProgramState` with the provided memory
+--   size of 1 byte, initialized to 0 (the standard is of 30,000 
+--   memory blocks).
+initialState :: Int -> String -> ProgramState
+initialState memSize program = MkState program cells 0
+  where cells = V.replicate memSize 0
 
-testingState :: ProgramState
-testingState = initialState $ replicate 65 '+' <> "."
+
+helloWorldProgram :: String
+helloWorldProgram = ">+++++++++[<++++++++>-]<.>+++++++[<++++>-]<+.+++++++..+++.[-] >++++++++[<++++>-] <.>+++++++++++[<++++++++>-]<-.--------.+++ .------.--------.[-]>++++++++[<++++>- ]<+.[-]++++++++++."
+
+testState :: Bool -> ProgramState -> IO ()
+testState printAll testSt = if printAll
+                            then execute testSt >>= print
+                            else void (execute testSt)
+
+helloWorld :: IO ()
+helloWorld = testState False $ initialState 10 helloWorldProgram
 
 
-
-test :: Bool -> IO ()
-test printAll = if printAll
-                then execute testingState >>= print
-                else execute testingState >> putStrLn ""
-
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
