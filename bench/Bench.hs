00771d3b86a1d45e7e1359fda95cec7a16dfedeb
0rphee <0rph3e@proton.me>
2023-01-25 22:20:08 -0600

    add benchmarking for numerous BF programs
    BF programs:
    helloworld.b
    196-commented.b
    392quine.b
    quine.b
    factor.b

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -1,6 +1,33 @@
-module Main where
+module Main (main) where
 
 import Criterion.Main
+import Brainhuck.NewInterpreter
+
+type FileName = String
+type Input = String
+type MemSize = Int
+
+genBenchmark :: (FileName, MemSize, Input) -> IO Benchmark
+genBenchmark (fileName, memSize, input) = do
+  strFromFile <- readFile $ "bf/" ++ fileName
+  pure $ bench ("INTERPRET: " ++ fileName) (whnfIO (tryToInterpret strFromFile memSize input))
+
+genListOfBenchmarks :: [(FileName, MemSize, Input)] -> IO [Benchmark]
+genListOfBenchmarks = mapM genBenchmark
+
 
 main :: IO ()
-main = defaultMain [bench "const" (whnf const ())]
+main = do
+  putStrLn ""
+  benchmarks <- genListOfBenchmarks [ ("helloworld.b", 30, " ")
+                                    , ("196-commented.b", 500,"923743\n")
+                                    , ("392quine.b", 400, " ")
+                                    , ("quine.b", 400, " ")
+                                    , ("factor.b", 400, "222222\n")
+                                    ]
+  defaultMain benchmarks
+
+
+
+
+
